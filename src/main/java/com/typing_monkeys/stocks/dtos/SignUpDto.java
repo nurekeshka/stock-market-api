b88a0c72bb6a57d85c07ebadf7c2ac7ac817d702
Nurbek <89794613+nurekeshka@users.noreply.github.com>
2025-05-16 18:52:58 +0200

    feat: add all the recent changes

--- src/main/java/com/typing_monkeys/stocks/dtos/SignUpDto.java
+++ src/main/java/com/typing_monkeys/stocks/dtos/SignUpDto.java
@@ -31,4 +31,16 @@ public class SignUpDto {
     return password;
   }
 
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
 }
--- src/main/java/com/typing_monkeys/stocks/services/AccountService.java
+++ src/main/java/com/typing_monkeys/stocks/services/AccountService.java
@@ -1,32 +1,61 @@
 package com.typing_monkeys.stocks.services;
 
+import java.security.Key;
+
+import javax.crypto.spec.SecretKeySpec;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.typing_monkeys.stocks.configs.JwtConfig;
+import com.typing_monkeys.stocks.dtos.SignInDto;
 import com.typing_monkeys.stocks.dtos.SignUpDto;
 import com.typing_monkeys.stocks.models.Account;
 import com.typing_monkeys.stocks.repositories.AccountRepository;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
 @Service
 public class AccountService {
-  private final AccountRepository repository;
-  private final PasswordEncoder encoder;
+  private final AccountRepository accountRepository;
+  private final PasswordEncoder passwordEncoder;
+  private final AuthenticationManager authenticationManager;
+  private final JwtConfig jwtConfig;
 
   @Autowired
-  public AccountService(AccountRepository accountRepository, PasswordEncoder passwordEncoder) {
-    this.repository = accountRepository;
-    this.encoder = passwordEncoder;
+  public AccountService(AccountRepository accountRepository, PasswordEncoder passwordEncoder,
+      AuthenticationManager authenticationManager, JwtConfig jwtConfig) {
+    this.accountRepository = accountRepository;
+    this.passwordEncoder = passwordEncoder;
+    this.authenticationManager = authenticationManager;
+    this.jwtConfig = jwtConfig;
   }
 
   public Account createAccount(SignUpDto signUpDto) {
     Account account = new Account.Builder()
         .username(signUpDto.getUsername())
-        .password(encoder.encode(signUpDto.getPassword()))
+        .password(passwordEncoder.encode(signUpDto.getPassword()))
         .email(signUpDto.getEmail())
         .build();
 
-    return this.repository.save(account);
+    return this.accountRepository.save(account);
+  }
+
+  public String signIn(SignInDto signInDto) {
+    authenticationManager.authenticate(
+        new UsernamePasswordAuthenticationToken(signInDto.getUsername(), signInDto.getPassword()));
+
+    byte[] keyBytes = jwtConfig.getSecret().getBytes();
+    Key key = new SecretKeySpec(keyBytes, SignatureAlgorithm.HS512.getJcaName());
+
+    return Jwts.builder()
+        .setSubject(signInDto.getUsername())
+        .signWith(key, SignatureAlgorithm.HS512)
+        .compact();
   }
 
 }
